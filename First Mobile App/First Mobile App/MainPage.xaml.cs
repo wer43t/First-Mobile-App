851f8a5e33595b656d94b3bc78d6adaaef483664
abrarov03 <abrarov03@gmail.com>
2022-02-09 17:35:25 +0300

    Добавьте файлы проекта.

---
+++ First Mobile App/First Mobile App/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace First_Mobile_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnRegistration_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageRegistration());
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ First Mobile App/First Mobile App/PageRegistration.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace First_Mobile_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageRegistration : ContentPage
+    {
+        public PageRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
